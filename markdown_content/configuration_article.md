5b993ac436897a205b66658a03a4ce8195da04cb
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-11-24 16:16:09 +0900

    Many changes, check description
    * Added the ability to add custom configuration to markdown articles! check util::interpret_markdown_configurations
    * Added some notes on markdown styles and how they work
    * Added bootstrap to default index

---
+++ markdown_content/configuration_article.md
@@ -0,0 +1,10 @@
+# Using configurations inside an article!
+
+```json
+/*interpret*/
+{
+    "date": "24/11/2022"
+}
+```
+
+This article has a date!
--- src/util.js
+++ src/util.js
@@ -25,3 +25,53 @@ async function get_markdown_content_as_html(name) {
     const marked = window["marked"];
     return marked.parse(markdown_content);
 }
+
+// Given an element inside a markdown content, returns the parent Article tag
+// It wont work if the markdown article is not inside an article tag, example:
+// 
+//     <article id="1" class="markdown-body"></article>
+//     document.getElementById('1').innerHTML = await get_markdown_content_as_html("content");
+// 
+function find_parent_article(element) {
+    let parent = element.parentElement;
+    while (parent.tagName !== 'ARTICLE') {
+        parent = parent.parentElement;
+    }
+    return parent;
+}
+
+// This function will look for a piece of markdown like this one:
+// 
+//     ```json
+//     /*interpret*/
+//     {
+//         "whatever": "you want",
+//         "just make sure": "its valid json format"
+//     }
+//     ```
+// 
+// It will find it, parse the json content in there and execute the input parameter
+// function `config_handler`, giving you the article element and the content of the json.
+// The markdown blocks themselves will be hidden!
+function interpret_markdown_configurations(config_handler) {
+    let pre_codes = document.querySelectorAll("pre code");
+    for (let i = 0; i < pre_codes.length; i++) {
+        let pre_code = pre_codes[i];
+        
+        try {
+            if (pre_code.textContent.startsWith('/*interpret*/')) {
+                pre_code.style.display = "none";
+                let length_of_header = '/*interpret*/\n'.length;
+                let config_text = pre_code.textContent.substring(length_of_header);
+                let config = JSON.parse(config_text);
+                let article_element = find_parent_article(pre_code);
+                if (article_element) {
+                    config_handler(article_element, config);
+                }
+            }
+        }
+        catch (exception) {
+            console.log("There was an error interpreting the configurations of a markdown article");
+        }
+    }
+}
