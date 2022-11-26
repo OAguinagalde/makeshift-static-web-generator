c6c0050ce0c31fb60d88be90baaf602e10adff08
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-11-27 01:00:43 +0900

    Big changes but mostly wip

--- compile.js
+++ compile.js
@@ -44,19 +44,25 @@ async function map_recursive(item, f) {
     }
 };
 
-const regexp = /<<<(md|html):(.+)>>>|\{\{\{(.+)\}\}\}/;
+const regexp = /<<<(md|html|c):(.+)>>>|\{\{\{(.+)\}\}\}/;
 
-async function parse(file_path, options) {
+async function parse(input, is_file, options) {
 
-    console.log(`parsing template: ${file_path}`);
-    const file_content = await fs.promises.readFile(file_path);
-    const lines = file_content.toString().split('\n');
+    let data_to_parse;
+    if (is_file) {
+        console.log(`parsing template: ${input}`);
+        data_to_parse = await fs.promises.readFile(input);
+    }
+    else {
+        console.log(`parsing text: ${input.substring(0,50).replace(/(?:\r\n|\r|\n)/g,"")}...`);
+        data_to_parse = input;
+    }
+    const lines = data_to_parse.toString().split('\n');
 
     for (let i = 0; i < lines.length; i++) {
 
         for (let match = lines[i].match(regexp), result; match;) {
             
-            // <<<[md|html].+>>> for files
             if (match[0][0] === '<') {
                 
                 const relative_file = match[2].trim();
@@ -64,27 +70,38 @@ async function parse(file_path, options) {
                 switch (match[1]) {
                     
                     case 'md': {
-                        console.log(`inserting md: ${relative_file}`);
                         const md = await fs.promises.readFile(relative_file, 'utf8');
                         const md_parsed = await marked.parse(md);
                         result = md_parsed;
                     } break;
 
+                    case 'c': {
+                        if (relative_file === 'inner') {
+                            result = '<innermarker></innermarker>'
+                        }
+                    } break;
                     
                     case 'html': {
-                        result = await parse(relative_file, options);
+                        result = await parse(relative_file, true, options);
                     } break;
 
                 }
             }
 
-            // {{{.+}}} for variables
             else {
-                const variable_to_replace = match[3].trim();
-                result = options[match[3].trim()];
-                console.log(`replacing variable: ${variable_to_replace} => ${options[variable_to_replace]}`);
+                const content = match[3].trim();
+                const [ variable, index, sub_variable ] = content.split(":", 3);
+                
+                if (index && sub_variable) {
+                    
+                    result = options[variable][index][sub_variable];
+                }
+                else {
+                    result = options[variable];
+                }
             }
 
+            console.log(`replacing ${match[0]} with ${result.substring(0, 50).replace(/(?:\r\n|\r|\n)/g,"")}...`);
             lines[i] = lines[i].replace(match[0], result ? result : '');
             match = lines[i].match(regexp);
         
@@ -127,13 +144,6 @@ async function build_project(deps, pages) {
         }
     }
 
-    // Process articles
-    let markdown_files = [];
-    await map_recursive('./articles', async (file) => {
-        const file_name = path.parse(file).name;
-        markdown_files.push({id: file_name, file: file});
-    });
-
     // Process HTML files.
     // Basically take all HTML files and apply the changes required where necessary.
     // 1. Embed the Markdown content directly into the html
@@ -148,20 +158,75 @@ async function build_project(deps, pages) {
         let modified = false;
         let $ = cheerio.load(html);
         
-        // embed markdown contents in articles
-        for (let article of $('article')) {
-            const id = article.attribs['id'];
-            const markdown_file = id && markdown_files.find((item) => item.id === id)
-            if (markdown_file) {
-                const md = await fs.promises.readFile(markdown_file.file, 'utf8');
-                const md_parsed = await marked.parse(md);
-                $(md_parsed).appendTo(article);
-                modified = true;
+        // Find every tag of type article and do whatever needs to be done with it
+        for (let article = $('article').first(); article;) {
+        
+            // mark the article as handled, so that it is not handled twice
+            $(article).attr('handled', 'true');
+        
+            // For now, the id of the article is expected to be a relative path to an actual file
+            // of type json or md. Example: <article id="some/file.json"/>
+            const file_path = $(article).attr('id');
+            if (fs.existsSync(file_path)) {
+            
+                // Parse the file name to be able to to get its extension
+                const file = path.parse(file_path);
+                switch (file.ext) {
+                    
+                    // If its a markdown file:
+                    // 1. load the file
+                    // 2. check if it has any special tag <<<...>>> or {{{...}}} and replace it
+                    // 3. conver it into html
+                    // 4. put it back into the final html page
+                    case ".md": {
+                        const md = await fs.promises.readFile(file_path, 'utf8');
+                        const template_file_parsed = await parse(md, false, {})
+                        const md_parsed = await marked.parse(template_file_parsed);
+                        
+                        const inner_elements = $(article).html();
+                        $(article).children().remove()
+                        $(md_parsed).appendTo(article);
+
+                        // Used for when we want to keep the inner html of the article tag: <article> innerstuff </article>
+                        $('innermarker').replaceWith(inner_elements)
+
+                        modified = true;
+                    } break;
+                    
+                    // If its a json file:
+                    // 1. load the file
+                    // 2. parse the json
+                    // 3. it should contain both a "template" and a "config" fields
+                    // 4. Load the template file and pass the config as options.
+                    // 5. put it back into the final html page
+                    case ".json": {
+                        const json = await fs.promises.readFile(file_path, 'utf8');
+                        const json_parsed = await JSON.parse(json);
+                        const template_name = json_parsed.template;
+                        const config = json_parsed.config;
+                        const template_file_parsed = await parse(template_name, true, config)
+                        $(template_file_parsed).appendTo(article);
+                        modified = true;
+                    } break;
+                }
+                
+            }
+
+            // check if there is any article tag left to handle
+            const left_to_handle = $('article').filter(function (i, el) {
+                return $(this).attr('handled') !== 'true';
+            })
+
+            if (left_to_handle.length > 0) {
+                article = left_to_handle.first();
+            }
+            else {
+                break;
             }
         }
 
-        // process templates
-        const length_of_header = '/*template*/\n'.length;
+        // process templates embeded in markdown files
+        const length_of_header = '/*template*/'.length;
         for (let pre_code of $('pre code')) {
             const inner_html = $(pre_code).html()
             if (inner_html.startsWith('/*template*/')) {
@@ -169,7 +234,6 @@ async function build_project(deps, pages) {
                 const template_json = JSON.parse(template_text);
                 let handled = false;
 
-                console.log(`processing template: ${template_json.template}`);
                 switch (template_json.template) {
                     
                     case "fancy_subtitle": {
@@ -183,7 +247,7 @@ async function build_project(deps, pages) {
                         // needs to be parsed with the function `parse` above
                         const template_name = template_json.template;
                         const config = template_json.config;
-                        const template_file_parsed = await parse(template_name, config)
+                        const template_file_parsed = await parse(template_name, true, config)
                         $($(pre_code).parent()).replaceWith($(template_file_parsed));
                         handled = true; modified = true;
                     } break;
